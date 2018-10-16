85c897b1c75d254b0892521b5dd11bc6eb681150
Joe Preddy <joe.preddy@gmail.com>
2018-10-16 07:59:01 +0100

    First queries - video 14

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,14 @@
-const Query = {}
+const { forwardTo } = require('prisma-binding')
+
+const Query = {
+
+  items: forwardTo('db'),
+
+  // Longhanded way - forward if there's no additional logic to be done
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items()
+  //   return items
+  // },
+}
 
 module.exports = Query
